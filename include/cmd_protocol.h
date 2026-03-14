cb6787d09feca46b31dd343aa0cf854ac87681fb
jett <jhendrick196@gmail.com>
2026-03-14 12:35:06 -0400

    updating CMD protocol.h

--- include/cmd_protocol.h
+++ include/cmd_protocol.h
@@ -4,35 +4,62 @@
 
 // Enum for different commands
 enum RobotCommandType : uint8_t {
+    //command for doing a controlled stop of the robot, cutting power to the drives
     CSTOP_Command = 0x01,
+    //command for Emergency stoping the robot, cutting power to drives.
     ESTOP_Command = 0x02,
     //command for moving the robot
     MOVE_Command = 0x03, 
-    //command for controling the relays
+    //command for controling the relays.
     RELAY_Command = 0x04,
-    //commands that can change settings or manually control a system
-    SYTEM_Command = 0x05,
+    //commands that can change settings or manually control a system.
+    SYSTEM_Command = 0x05,
+    //commands that requests the status of the robot.
+    INFO_REQUEST_Command = 0x06,
 };
 
 // Enum for movement cmd types
 enum MoveCMDType : uint8_t {
     // Drive cmds
+    //sets drive motors to 0 and breaks
     STOP_Command = 0x01,
+    //sets the robot to drive a calculated distance Forward using the encoders and MPU6050
     DIR_FORWARD = 0x02,
+    //sets the robot to drive a calculated distance Backward using the encoders and MPU6050
     DIR_BACKWARD = 0x03,
-    // Turn cmds
+    //sets the robot to turn to the Right to a calculated angle using the encoders and MPU6050
     DIR_TURN_RIGHT = 0x04,
+    //sets the robot to turn to the Left to a calculated angle using the encoders and MPU6050
     DIR_TURN_LEFT = 0x05,
-    // Tank drive
+    // Tank drive setting two values to set the left and right motors to
     DIR_TANK = 0x06,
     
 };
 
 // Enum for relay cmd types
 enum RelayCMDType : uint8_t {
+    //sets all relays to closed
     ALL_Closed = 0x01,
+    //sets all relays to open
     ALL_Open = 0x02,
+    //sets a single relay's state
     Relay_Num = 0x03,
+};
+
+// Enum for system cmd types
+enum SystemCMDType : uint8_t {
+    //
+    RadioRST = 0x01,
+    //
+    CANBusRST = 0x02,
+    //Clicks all 
+    RelayCLKTest = 0x03,
+    //Blinks both motor controllers to verify comms with CAN0
+    MotorControllerBLINK = 0x04,
+    //resets and calibrates the gyro
+    GYRORST = 0x05,
+    //resets the motor encoders to zero
+    EncoderRST=0x06,
 };
 
 
